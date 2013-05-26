45a6995d6cc648dce0e37e0c69d3407eb72fa547
josh <josh@josh-ThinkPad-T410.(none)>
2013-05-25 17:18:07 -0700

    signal handler with all the signals

--- shell_signals.c
+++ shell_signals.c
@@ -7,5 +7,20 @@ void signal_handler(int signal)
 	if(signal == SIGCHLD){
 		waitpid(0,&status,0);
 	}
-	
+	else if(signal == SIGTIN){
+		printf("Recieved SIGTIN");
+	}
+	else if(signal == SIGTOU){
+		printf("Recieved SIGTOU");
+	}
+	else if(signal == SIGTERM,){
+		printf("Recieved SIGTERM");
+	}	
+	else if(signal == SIGTSTP){
+		printf("Recieved SIGTSTP");
+	}
+	else if(signal == SIGCONT){
+		printf("Recieved SIGCONT");
+	}	
+			
 }
