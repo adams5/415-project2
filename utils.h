afcc9528b846cfbe2863f3fc111854ea98f63cce
StuPidasso <stupidasso@ubuntu.(none)>
2013-05-27 10:41:31 -0700

    added struct for fg proc

--- utils.h
+++ utils.h
@@ -1,9 +1,15 @@
 
-struct BGProc{
+typedef struct BGProc{
 	pid = 0;
 	pgid = 0;
 	BGProc next = NULL;
-};
+}bgproc;
+
+typedef struct FGProc{
+	pid = 0;
+	pgid = 0;
+	FGProc next = NULL;
+}fgproc;
 
 
 int cmp(char *s1, char *s2);
